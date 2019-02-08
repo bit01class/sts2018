aa27e9c210b3da7872f61a9aec08361fb24de515
bit01class <bit01class@gmail.com>
2019-02-08 18:25:02 +0900

    spring ibatis -> mybatis

--- sts07/src/main/java/com/bit/sts07/model/Bbs2DaoImpl.java
+++ sts07/src/main/java/com/bit/sts07/model/Bbs2DaoImpl.java
@@ -5,39 +5,69 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 import com.bit.sts07.model.entity.Bbs2Vo;
-import com.ibatis.sqlmap.client.SqlMapClient;
 
 @Repository
 public class Bbs2DaoImpl implements Bbs2Dao {
 	@Inject
-	SqlMapClient sqlMapClient;
+	SqlSession session;
 
 	@Override
 	public List<Bbs2Vo> selectAll() throws SQLException {
-		return sqlMapClient.queryForList("selectAll");
+		return session.selectList("bbs.selectAll");
 	}
 
 	@Override
 	public Bbs2Vo selectOne(int idx) throws SQLException {
-		return (Bbs2Vo) sqlMapClient.queryForObject("selectOne",idx);
+		return session.selectOne("bbs.selectOne",idx);
 	}
 
 	@Override
 	public void insertOne(Bbs2Vo bean) throws SQLException {
-		sqlMapClient.insert("insertOne",bean);
+		session.insert("bbs.insertOne",bean);
 	}
 
 	@Override
 	public int updateOne(Bbs2Vo bean) throws SQLException {
-		return sqlMapClient.update("updateOne", bean);
+		return session.update("bbs.updateOne", bean);
 	}
 
 	@Override
 	public int deleteOne(int idx) throws SQLException {
-		return sqlMapClient.delete("deleteOne", idx);
+		return session.delete("bbs.deleteOne", idx);
 	}
+	
+///////////////////////  ibatis //////////////////////////////////////	
+//	@Inject
+//	SqlMapClient sqlMapClient;
+//
+//	@Override
+//	public List<Bbs2Vo> selectAll() throws SQLException {
+//		return sqlMapClient.queryForList("selectAll");
+//	}
+//
+//	@Override
+//	public Bbs2Vo selectOne(int idx) throws SQLException {
+//		return (Bbs2Vo) sqlMapClient.queryForObject("selectOne",idx);
+//	}
+//
+//	@Override
+//	public void insertOne(Bbs2Vo bean) throws SQLException {
+//		sqlMapClient.insert("insertOne",bean);
+//	}
+//
+//	@Override
+//	public int updateOne(Bbs2Vo bean) throws SQLException {
+//		return sqlMapClient.update("updateOne", bean);
+//	}
+//
+//	@Override
+//	public int deleteOne(int idx) throws SQLException {
+//		return sqlMapClient.delete("deleteOne", idx);
+//	}
 
 }
